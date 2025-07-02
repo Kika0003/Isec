537851c9030ffb54c864a71a89c8b0be5725436b
CarolinaVeloso <97366635+Kika0003@users.noreply.github.com>
2025-07-02 18:35:38 +0100

    Delete TP_LS directory

--- TP_LS/minesweeper-game-react/src/components/index.js
+++
@@ -1,9 +0,0 @@
-export {default as Footer} from "./footer/footer.component";
-export {default as ControlPanel} from "./control-panel/control-panel.component";
-export {default as Board} from "./Board/board.component";
-export {default as Square} from "./Square/Square.component";
-export {default as CreateBoard} from "./util/GameUtil";
-export {default as Game} from "./game-panel/game-panel.component";
-export {default as Taskbar} from "./taskbar/taskbar.component";
-export {default as Menu} from "./Menu/menu.component";
-export {default as GameOver} from "./GameOver/game-over";
